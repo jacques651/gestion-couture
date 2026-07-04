553125e1a1fb600ae5a54e283c423fb43364d9cb
Jacques KORGO <jacqueskorgo5@gmail.com>
2026-07-04 11:46:50 +0000

    Retire le build et le patch du suivi Git

---
+++ backend/public/assets/IconClock-CEOWTz1r.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-BA-Rp2mN.js";const c=[["path",{d:"M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0",key:"svg-0"}],["path",{d:"M12 7v5l3 3",key:"svg-1"}]],e=o("outline","clock","Clock",c);export{e as I};
