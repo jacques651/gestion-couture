cc003e261a23b51cf300da9dd37199f854a9e15b
Jacques KORGO <jacqueskorgo5@gmail.com>
2026-07-04 11:46:13 +0000

    Normalise les fins de ligne

--- backend/public/assets/IconBook-B0IUyXBw.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-BA-Rp2mN.js";const a=[["path",{d:"M3 19a9 9 0 0 1 9 0a9 9 0 0 1 9 0",key:"svg-0"}],["path",{d:"M3 6a9 9 0 0 1 9 0a9 9 0 0 1 9 0",key:"svg-1"}],["path",{d:"M3 6l0 13",key:"svg-2"}],["path",{d:"M12 6l0 13",key:"svg-3"}],["path",{d:"M21 6l0 13",key:"svg-4"}]],e=o("outline","book","Book",a);export{e as I};
